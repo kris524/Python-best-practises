f7719728cbb677478fcb62077fae907470ed9e46
Kris <kristian_dilov@abv.bg>
2022-10-18 19:53:03 +0100

    initial commit

--- src/first_module/main.py
+++ src/first_module/main.py
@@ -1,4 +1,44 @@
 from abc import ABC, abstractmethod
 
-class Creator(ABC):
+class Main(ABC):
     
+    @abstractmethod
+    def factory_method(self):
+        """the subclasses of the Main class will create other classes using this method"""
+        pass
+
+    def some_operation(self) -> str:
+        product = self.factory_method()
+        result = f"Creator: The same creator's code has just worked with {product.operation()}"
+        return result
+
+
+class Product(ABC):
+
+    @abstractmethod
+    def operation(self)-> str:
+        pass
+
+
+# These are the specific Factoties, they return a abstract product in general so they stay indep
+class SpecificProductCreator1(Main):
+    def factory_method(self) -> Product:
+        return SpecificProduct1()
+
+class SpecificProductCreator2(Main):
+    def factory_method(self) -> Product:
+        return SpecificProduct2()
+
+
+# These are the specific Products
+class SpecificProduct1(Product):
+    def operation(self) -> str:
+        return "This is Specific Product 1"
+
+
+class SpecificProduct2(Product):
+    def operation(self) -> str:
+        return "This is Specific Product 2"
+
+
+
